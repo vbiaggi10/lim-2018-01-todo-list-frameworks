c4e810fbcbbbc6524bca338a717e61304d219dbd
vbiaggi10 <vbiaggi10@gmail.com>
2018-10-02 17:30:47 -0500

    remove long paths

--- src/components/dash/components/home/components/PaintList.js
+++ src/components/dash/components/home/components/PaintList.js
@@ -69,7 +69,7 @@ class PaintList extends Component {
             disabled="disabled"
           />
           <Link
-            to="/lim-2018-01-todo-list-frameworks/tasks/"
+            to="/tasks/"
             onClick={() =>{this.handleInput(this.id)}}
           >
             See tasks
