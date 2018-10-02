0c61a3b12212a917f19c99795317b29bd1efc528
Valeria <vbiaggi10@gmail.com>
2018-10-02 12:24:24 -0500

    routes

--- src/components/dash/components/wall/components/PaintList.js
+++ src/components/dash/components/wall/components/PaintList.js
@@ -69,7 +69,7 @@ class PaintList extends Component {
             disabled="disabled"
           />
           <Link
-            to="/tasks"
+            to="//lim-2018-01-todo-list-frameworks/tasks"
             onClick={() =>{this.handleInput(this.id)}}
           >
             See tasks
